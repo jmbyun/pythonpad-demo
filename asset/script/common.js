a78cc40c02d28182bef311771769a46aa183c1fd
Jeongmin Byun <jmbyun91@gmail.com>
2020-08-07 16:34:42 +0900

    implement pad and doc

---
+++ asset/script/common.js
@@ -0,0 +1,30 @@
+function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray(prmstr) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for (var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+function downloadTextAsFile(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+window.getSearchParameters = getSearchParameters;
+window.downloadTextAsFile = downloadTextAsFile;
